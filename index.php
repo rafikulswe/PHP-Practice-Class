7ba325b74db693f906ef26a9a487654899176f66
rafiswe <rafikulislam521@gmail.com>
2021-10-19 22:12:03 +0600

    2nd lecture about variables & dataTypes

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+
+    $x = 100; // integer number
+    $w = 10.5; // decimal/double/float number
+    $z = 'Jarif'; // String
+    $decesion = false; // boolean
+    echo gettype($decesion);
+
+
+
+
+
+    // 1. How php works
+    // 2. How to run php file in browser
+    // 3. How to print any text
+    // 4. How to comment in php file
+    // 5. Why we use php end tag & when
+    // 6. php code will never show in browser
+    
+
+    // 7. Variable Declaration Rules
+    // start with $ sign
+    // variable don't start with 0-9 but number can be used on the last of variable
+    // Don't give any spaces in variable declarations
+    // 8. Data type in php.
