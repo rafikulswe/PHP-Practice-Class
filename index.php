2bdfa4e156b41672239c474ac51c778c5afac425
rafiswe <rafikulislam521@gmail.com>
2021-10-24 00:59:36 +0600

    lecture 2 about data types & constant variable

--- index.php
+++ index.php
@@ -1,14 +1,30 @@
 <?php 
 
+
     $x = 100; // integer number
     $w = 10.5; // decimal/double/float number
-    $z = 'Jarif'; // String
+    $z = 'Jarif  '; // String
     $decesion = false; // boolean
-    echo gettype($decesion);
+    $age = 15;
+    // echo gettype($decesion);
+    echo "<br>";
 
+    // Compound Data type -> array & object
+    $name = [$x, $w, $z]; // array
+    // echo gettype($name);
+    echo "<br>";
+    var_dump($name); 
+    echo "<br>";
 
+    // Constant variable
+    define('authorName', 'Jarif Zawad', true);
+    echo AUthorNAME;
 
-
+    echo "<br>";
+    // concat
+    echo authorName.' is an author and he is '.$age.' years old';
+    echo "<br>";
+    echo authorName.' read in class 9 and he is '.$age.' years old';
 
     // 1. How php works
     // 2. How to run php file in browser
@@ -23,3 +39,5 @@
     // variable don't start with 0-9 but number can be used on the last of variable
     // Don't give any spaces in variable declarations
     // 8. Data type in php.
+    // Commonly used scalar data type (4)
+    // Compound Data type
