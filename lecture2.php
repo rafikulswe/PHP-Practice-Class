066609cca17018e26e4ab735b27d4ebabbdbdcb7
rafiswe <rafikulislam521@gmail.com>
2021-11-10 21:38:53 +0600

    lecture 2

---
+++ lecture2.php
@@ -0,0 +1,49 @@
+<?php
+
+// Arithmatic operators
+// +
+// -
+// *
+// /
+// %
+// **
+// $numOne = 2;
+// $numTwo = 4;
+//  2 ) 11 ( 5
+//      10
+//     ----
+//       1
+// echo $numOne ** $numTwo; 
+// 2^4 = 2 * 2 * 2 * 2
+
+
+// Assignment (নির্ধারণ অপারেটর)
+//  =
+//  +=
+//  -=
+//  *=
+//  /=
+//  %=
+// $total_amount = 20; // 2 jon calculation
+// $numThree = 4;
+// $total_amount %= $numThree;
+// echo $total_amount;
+
+
+// Increment / decrement (বৃদ্ধি / হ্রাস)
+// ++
+// --
+// $iteration = 10;
+
+// echo ++$iteration;
+// $i=1971;
+// for (;;) { 
+//     echo $i;
+//     $i++;
+//     echo '<br>';
+// }
+
+// Logical (যৌক্তিক)
+// &&
+// ||
+
